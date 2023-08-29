ec00ba12ac91ecc98cfd9577d07101043e9f8054
li88qq <573258011@qq.com>
2023-08-29 22:03:04 +0800

    调整dev开发中页面

--- src/views/dev.vue
+++ src/views/dev.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    开发中
-  </div>
+  <a-result status="warning" title="开发中...">
+    <template #extra>
+      <a-button type="primary">返回首页</a-button>
+    </template>
+  </a-result>
 </template>
 <script setup lang="ts">
 import {} from 'vue'
